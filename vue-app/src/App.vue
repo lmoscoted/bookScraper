f550c8b8417c0daf49ac5f5a5d2591f9b4a4f9ad
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-18 00:57:53 -0300

    Update the app view with header

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <keep-alive include="home">
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
+
+<script>
+import Header from './components/layout/Header'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
