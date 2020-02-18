5aa37676339c7690f7967882268230d5d1cbc9af
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 21:09:34 -0300

    Create the Book model

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -8,3 +8,15 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
             return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    product_description = models.CharField(max_length=2000)
+    price = models.CharField(max_length=50) 
+    category = models.ForeignKey(Category, related_name='books',on_delete=models.CASCADE) # Foreign Key 
+    upc = models.CharField(max_length=50)
+    stock = models.PositiveIntegerField() # Positive Integer
+    thumbnail = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name            
