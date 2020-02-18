c958892dc7ec57f03e537e2424372f3c882abf85
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-18 02:12:40 -0300

    Add filter function

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -32,7 +32,7 @@
         <button class="btn btn-danger btn-sm" @click="deleteCategory( props.row.id )"> Delete</button>
       </span>
       <span v-else-if="props.column.field == 'books'">
-        <button class="btn btn-primary btn-sm" @click="showBooks( props.row )"> Books</button>
+        <button class="btn btn-primary btn-sm" @click="showBooks( props.row )"> Get</button>
       </span>
       <span v-else-if="props.column.field == 'name'">
         <span style="font-weight: bold;">{{props.row.name}}</span>
@@ -54,11 +54,61 @@
 // import the styles
 import 'vue-good-table/dist/vue-good-table.css'
 import { VueGoodTable } from 'vue-good-table'
+import axios from 'axios'
+
+// const urlCategories = 'django-server:8000/categories/'
+// const urlCategories = 'http://192.168.99.100:8000/categories/'
+const urlCategories = 'http://127.0.0.1:8000/categories/'
 
 export default {
   name: 'Home',
   components: {
     VueGoodTable
+  },
+  data () {
+    return {
+      columns: [
+
+        {
+          label: 'Category Name',
+          field: 'name',
+          // type: 'number',
+          filterOptions: {
+            enabled: true, // enable filter for this column
+            placeholder: 'Search category', // placeholder for filter input
+            filterValue: '', // initial populated value for this filter
+            filterFn: this.filterName// custom filter function that
+          }
+        },
+        {
+          label: 'Books',
+          field: 'books'
+        },
+        {
+          label: 'Action',
+          field: 'btn',
+          html: true
+        }
+      ],
+      rows: []
+    }
+  },
+  methods: {
+    getData: async function () {
+      await axios.get(urlCategories)
+        .then(res => { this.rows = res.data })
+        .catch(err => console.log(err))
+    },
+    filterName: function (data, filterString) {
+      filterString = filterString.toLowerCase().trim()
+      data = data.toLowerCase().trim()
+      if (filterString.length > 0) {
+        if (data.indexOf(filterString) !== -1) {
+          return true
+        }
+      }
+    }
+
   }
 }
 </script>
