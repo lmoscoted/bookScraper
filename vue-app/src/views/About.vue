e051368c41abd6f198758d993c8cd15bf3cf9111
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-18 03:53:03 -0300

    Add info to the about page

--- vue-app/src/views/About.vue
+++ vue-app/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div >
+    <div>
+  <b-card title="BookScraper" sub-title="Techk Test">
+    <b-card-text>
+      BookScraper is a Vue web application which allows to get data through a scraper  and show in on a table,
+      using an API built in Django Rest Framework.
+    </b-card-text>
+
+    <b-link href="http://books.toscrape.com/index.html" class="card-link">Click here to see the website</b-link>
+  </b-card>
+</div>
+
+</div>
 </template>
