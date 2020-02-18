d6fc565c3a89f30533eb857119f32af566ba022b
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 21:28:20 -0300

    Create the skeleton for the class view

--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -6,3 +6,26 @@ from rest_framework import viewsets, generics, mixins
 
 from .models import Category, Book
 from .serializer import CategorySerializer, BookSerializer
+
+
+# Class view for only list, delete categories
+class CategoryView(mixins.DestroyModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+
+    serializer_class = CategorySerializer 
+    queryset = Category.objects.all()                
+
+
+# Class view for only delete books
+class BookView(mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
+
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+# Class view for only list the books from
+# a specific category
+class CategoryBooksView(generics.ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
