1e097154bed9b92d82945f835f3d9018e50684e5
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 22:40:57 -0300

    Add the functions to get the book details

--- techk/apps/scraper/services.py
+++ techk/apps/scraper/services.py
@@ -30,6 +30,7 @@ def get_content_bs4(url):
         print(e)
         return None 
 
+
 def get_categories(url):
     """ Get all categories both id and names  """
     soup= get_content_bs4(url)
@@ -63,3 +64,45 @@ def get_urls_publicacion(url):
         url_paginas = []
     finally:
         return url_paginas        
+
+
+def get_category_id(url_category):
+    """ Get the category id from the url category """
+
+    category_part = url_category.split('/')[-2]
+    category_id = int(category_part.split('_')[-1]) -1
+    return category_id
+
+
+def get_book_details(url_book,url_base_catalogue):
+    '''
+    Get the description, upc, stock, category_id
+    from the given book's url.
+    '''
+    book_detail_soup = get_content_bs4(url_book)
+    book_details= []
+    if book_detail_soup:
+        description = book_detail_soup.select('.product_page p')[3].get_text(strip=True)
+        stock_ = book_detail_soup.select('.product_page p')[1].get_text()
+        stock = int(stock_.strip().split(' ')[2].replace('(',''))
+        upc = get_upc(book_detail_soup)
+        url_category_ = book_detail_soup.select('.breadcrumb a')[2]['href']
+        url_category =  url_category_.replace('../',url_base_catalogue)
+        category_id = get_category_id(url_category)
+        #return category_id
+        #stock = book_detail_soup.select('.')
+
+        book_details = [description,stock,upc,category_id]#[description,upc,stock,category_id]
+                        
+    return book_details
+
+def get_upc(book_detail_soup):
+    """ Get the book's upc from the book detail soup """
+    upc = ''
+    book_table = book_detail_soup.find('table', { 'class' : 'table table-striped' })
+    trs = book_table.find_all('tr')
+    #return trs
+    for tr in trs:
+        if tr.find('th').get_text(strip=True) == 'UPC':
+            upc = tr.find('td').get_text(strip=True)
+    return upc        
