f9391d377d5fd33b8ad971f2b9b05c4e74af25b4
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 21:22:40 -0300

    Import all dependencies for the views

--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets, generics, mixins
 
-# Create your views here.
+from .models import Category, Book
+from .serializer import CategorySerializer, BookSerializer
