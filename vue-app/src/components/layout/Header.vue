d351527a42d8908dff2d49d91ceb37c45cbf0f2d
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-18 00:54:23 -0300

    Create the header component

---
+++ vue-app/src/components/layout/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <b-navbar type="dark" variant="dark">
+        <b-navbar-brand  to="/">BookScraper</b-navbar-brand>
+        <b-navbar-nav align="end">
+        <b-nav-item to="/" >Home</b-nav-item>
+        <b-nav-item to="/about" >About</b-nav-item>
+        </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header {
+
+color:aliceblue;
+text-align: center;
+
+}
+</style>
