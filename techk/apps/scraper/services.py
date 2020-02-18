1fb2ffef90b2c50df82623a0b3c5896aaadcb141
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 22:37:50 -0300

    Add the get_urls_publicacion to the scraper service

--- techk/apps/scraper/services.py
+++ techk/apps/scraper/services.py
@@ -29,3 +29,24 @@ def get_content_bs4(url):
     except Exception as e:
         print(e)
         return None 
+
+
+
+def get_urls_publicacion(url):
+    '''
+    Get all the page url with 20 books/page
+    http://books.toscrape.com/catalogue/page-1.html  input 
+    '''
+    try:
+        browser = get_content_bs4(url)
+        libros_por_pagina = int(len(browser.select('.col-xs-6.col-sm-4.col-md-3.col-lg-3')))
+        numero_publicaciones = int(browser.select('.form-horizontal strong')[0].get_text()) 
+        total_paginas = math.ceil(numero_publicaciones/libros_por_pagina)
+        url_category_base = 'http://books.toscrape.com/catalogue/page-1.html'
+        url_paginas = [url_category_base.replace('page-1','page-'+str(pag)) 
+                        for pag in range(1,total_paginas+1)]
+    except Exception as e:
+        print(e)
+        url_paginas = []
+    finally:
+        return url_paginas        
