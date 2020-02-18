374261ac7d68adf9b6b564ecfccd239dfb37bf1c
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 22:27:19 -0300

    Fix url issue with scraper app

--- techk/config/urls.py
+++ techk/config/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, path
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('',include('apps/scraper.urls')),
+    path('',include('apps.scraper.urls')),
     url(r'^api-auth/', include('rest_framework.urls'))
 ]
