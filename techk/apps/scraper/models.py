e7d35bb7688e8ef959a54b8198ed9ade33a11891
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 21:08:22 -0300

    Create the category model

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+            return self.name
