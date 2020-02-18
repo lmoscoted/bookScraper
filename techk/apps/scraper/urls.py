a3ec015323d73987963bf6f1ccf1c1dfa83d200a
Luis Moscote <ing.luis.moscote@gmail.com>
2020-02-17 21:54:29 -0300

    update the url for the scraper app

--- techk/apps/scraper/urls.py
+++ techk/apps/scraper/urls.py
@@ -2,11 +2,17 @@ from django.urls import path, include, re_path
 from . import views
 from rest_framework import routers
 
-
+# Default router w
 router = routers.DefaultRouter()
+# Register the category url 
 router.register('categories',views.CategoryView, basename='Category')
+# Register the books category
 router.register('books',views.BookView, basename='Book')
 
 
 urlpatterns = [
-    path('',include(router.urls)),]
+    path('',include(router.urls)),
+    # Case for the books from categories
+    # e.g /categories/3/books
+    re_path(r'^categories/(?P<pk>[0-9]+)/books/$',  
+            views.CategoryBooksView.as_view()),]
